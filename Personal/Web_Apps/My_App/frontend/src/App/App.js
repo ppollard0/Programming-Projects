8e8be21bca1cd7cb5901b7c6858f0aa54663018a
ppollard0 <paulap726@outlook.com>
2025-06-14 18:05:57 -0500

    Added some basic web app elements

---
+++ Personal/Web_Apps/My_App/frontend/src/App/App.js
@@ -0,0 +1,16 @@
+import '../CSS/App.css';
+
+export default function App () {
+    const appInt = 1;
+    
+    return (
+        <div>
+            <div>
+                <p>Title</p>
+            </div>
+            <div>
+                <p>Body</p>
+            </div>
+        </div>
+    )
+}
